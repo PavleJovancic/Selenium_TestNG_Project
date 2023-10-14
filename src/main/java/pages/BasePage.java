a525728473f06364eeae9ebc2387acbae56a2fa2
PavleJovancic <drjovancicpavle@gmail.com>
2023-10-14 06:08:35 +0200

    Add base page

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+
+    public BasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+}
