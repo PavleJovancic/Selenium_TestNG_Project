69640bfa73610301eae42f8c86415a8a81fca5b2
PavleJovancic <drjovancicpavle@gmail.com>
2023-10-14 18:52:10 +0200

    Add base test

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.html5.WebStorage;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @BeforeMethod
+    public void navigateToBaseURL() {
+        driver.manage().deleteAllCookies();
+        driver.navigate().to(baseUrl);
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void deleteWebStorageData(){
+        WebStorage webStorage = (WebStorage) driver;
+        webStorage.getSessionStorage().clear();
+        webStorage.getLocalStorage().clear();
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
