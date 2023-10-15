d57ddcef1725b3d0e4ff27a9fa2b0c15077eb3a5
PavleJovancic <drjovancicpavle@gmail.com>
2023-10-15 14:07:45 +0200

    Add 'check input types' test to signup tests

--- src/main/java/pages/SignupPage.java
+++ src/main/java/pages/SignupPage.java
@@ -13,14 +13,26 @@ public class SignupPage extends BasePage{
     public WebElement getNameInputField(){
         return driver.findElement(By.id("name"));
     }
+    public String getNameInputFieldType(){
+        return getNameInputField().getAttribute("type");
+    }
     public WebElement getEmailInputField(){
         return driver.findElement(By.id("email"));
+    }
+    public String getEmailInputFieldType(){
+        return getEmailInputField().getAttribute("type");
     }
     public WebElement getPasswordInputField(){
         return driver.findElement(By.id("password"));
     }
+    public String getPasswordInputFieldType(){
+        return getPasswordInputField().getAttribute("type");
+    }
     public WebElement getConfirmPasswordInputField(){
         return driver.findElement(By.id("confirmPassword"));
+    }
+    public String getConfirmPasswordInputFieldType(){
+        return getConfirmPasswordInputField().getAttribute("type");
     }
     public WebElement getNSignMeUpButton(){
         return driver.findElement(By.cssSelector("button[type=submit]"));
--- src/test/java/tests/SignupTests.java
+++ src/test/java/tests/SignupTests.java
@@ -1,6 +1,8 @@
 package tests;
 
+import helpers.RetryAnalyzer;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -14,6 +16,17 @@ public class SignupTests extends BaseTest{
         wait
                 .withMessage("Signup button should redirect to signup page")
                 .until(ExpectedConditions.urlToBe(baseUrl + "/signup"));
+    }
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void checkInputTypes() {
+        Assert.assertEquals(signupPage.getNameInputFieldType(), "text",
+                "Name input type should be text");
+        Assert.assertEquals(signupPage.getEmailInputFieldType(), "email",
+                "Email input type should be email");
+        Assert.assertEquals(signupPage.getPasswordInputFieldType(), "password",
+                "Password input type should be password");
+        Assert.assertEquals(signupPage.getConfirmPasswordInputFieldType(), "password",
+                "Confirm password input type should be password");
     }
 
 
