a70f51e291ecfcf45618ec57d436591dd323c822
PavleJovancic <drjovancicpavle@gmail.com>
2023-10-17 12:21:46 +0200

    Add 'forbids visits to home url if not authenticated' to authroutes tests

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -6,5 +6,13 @@ import org.testng.annotations.Test;
 
 public class AuthRoutesTests extends BaseTest{
 
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidVisitsToHomeUrlIfNotAuthenticated(){
 
+        navigateToUrl(baseUrl + "/home");
+
+        wait
+                .withMessage("If not logged in page should redirect to login page")
+                .until(ExpectedConditions.urlToBe(baseUrl + "/login"));
+    }
 }
